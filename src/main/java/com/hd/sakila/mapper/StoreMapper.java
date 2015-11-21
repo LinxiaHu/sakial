1fa59842cf976f27d900ca82c968a0e464b190c9
LinxiaHu <chunyun@mail.ustc.edu.cn>
2015-11-21 16:32:40 +0800

    format

--- src/main/java/com/hd/sakila/mapper/StoreMapper.java
+++ src/main/java/com/hd/sakila/mapper/StoreMapper.java
@@ -6,25 +6,27 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface StoreMapper {
-    int countByExample(StoreExample example);
+	int countByExample(StoreExample example);
 
-    int deleteByExample(StoreExample example);
+	int deleteByExample(StoreExample example);
 
-    int deleteByPrimaryKey(Byte storeId);
+	int deleteByPrimaryKey(Byte storeId);
 
-    int insert(Store record);
+	int insert(Store record);
 
-    int insertSelective(Store record);
+	int insertSelective(Store record);
 
-    List<Store> selectByExample(StoreExample example);
+	List<Store> selectByExample(StoreExample example);
 
-    Store selectByPrimaryKey(Byte storeId);
+	Store selectByPrimaryKey(Byte storeId);
 
-    int updateByExampleSelective(@Param("record") Store record, @Param("example") StoreExample example);
+	int updateByExampleSelective(@Param("record") Store record,
+			@Param("example") StoreExample example);
 
-    int updateByExample(@Param("record") Store record, @Param("example") StoreExample example);
+	int updateByExample(@Param("record") Store record,
+			@Param("example") StoreExample example);
 
-    int updateByPrimaryKeySelective(Store record);
+	int updateByPrimaryKeySelective(Store record);
 
-    int updateByPrimaryKey(Store record);
+	int updateByPrimaryKey(Store record);
 }
