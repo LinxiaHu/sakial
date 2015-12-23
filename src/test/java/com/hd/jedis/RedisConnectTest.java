3379d029ec5f96e4f7c4971057960866bd4c212e
LinxiaHu <chunyun@mail.ustc.edu.cn>
2015-12-23 21:35:28 +0800

    add redis test

--- src/test/java/com/hd/jedis/RedisConnectTest.java
+++ src/test/java/com/hd/jedis/RedisConnectTest.java
@@ -1,5 +1,10 @@
 package com.hd.jedis;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -26,18 +31,49 @@ public class RedisConnectTest {
 		// jedis.set("key1", "val1");
 		// System.out.println(jedis.get("test1"));
 		// System.out.println(application);
-		
+
 		/* 采用声明式注入方式 */
 		ShardedJedisPool jedisPool = (ShardedJedisPool) application
 				.getBean("jedis");
 		ShardedJedis jedis2 = jedisPool.getResource();
-		System.out.println(jedis2.get("test1"));
-		jedis2.sadd("set1", "a");
-		byte val[] = new byte[3];
-		val[0] = 1;
-		val[1] = 2;
-		val[2] = 3;
-		jedis2.sadd("set2", val.toString());
+		// string测试
+//		 System.out.println(jedis2.get("test1"));
+//		 jedis2.sadd("set1", "a");
+//		 byte val[] = new byte[3];
+//		 val[0] = 1;
+//		 val[1] = 2;
+//		 val[2] = 3;
+//		 jedis2.sadd("set2", val.toString());
+
+		// set测试
+//		Set<String> set1 = jedis2.smembers("set1");
+//		for (String s : set1) {
+//			System.out.println(s);
+//		}
+
+		// list测试
+//		 jedis2.lpush("list1", "1");
+//		 jedis2.lpush("list1", "2");
+//		 List<String> list1 = jedis2.lrange("list1", 0, -1);
+//		 for (String l : list1) {
+//		 System.out.println(l);
+//		 }
+		
+		// map测试
+//		Map<String, String> map = new HashMap<String, String>();
+//		map.put("name", "zhangsan");
+//		map.put("age", "21");
+//		map.put("address", "beijing");
+//		jedis2.hmset("person1", map);
+//		List<String> person1 = jedis2.hmget("person1", "name", "age", "address");
+//		for (String p : person1) {
+//			System.out.println(p);
+//		}
+		
+		
+		
+		
+
 	}
 
 }
